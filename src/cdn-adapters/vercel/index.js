976373ccf55c5a261653aab1cb53f7feae1332de
Mike Chu <mikechu-optimizely@users.noreply.github.com>
2024-12-27 12:05:00 -0500

    build: for browsers instead of node

--- src/cdn-adapters/vercel/index.js
+++
@@ -1,5 +0,0 @@
-export { default as VercelAdapter } from './vercelAdapter';
-export { default as VercelKVInterface } from './vercelKVInterface';
-
-export * from './middleware';
-export * from './function';
