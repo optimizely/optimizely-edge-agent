248d56aee26a2aeb1eed4f66bb4838b900c49ba4
Mike Chu <mikechu-optimizely@users.noreply.github.com>
2025-01-03 14:56:22 -0500

    chore: remove legacy dir
    I'll use a .gitignored copy of `master` for Windsurf's ref

--- src/legacy/adapters/vercel/vercelAdapter.js
+++
@@ -1,4 +0,0 @@
-import * as optlyHelper from '../../utils/helpers/optimizelyHelper';
-import * as cookieDefaultOptions from '../../config/cookieOptions';
-import defaultSettings from '../../config/defaultSettings';
-import EventListeners from '../../core/providers/events/eventListeners';
